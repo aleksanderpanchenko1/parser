c233eb638b5a48d57125879f67868cf4af7f04f2
aleksanderpanchenko1 <31216083+aleksanderpanchenko1@users.noreply.github.com>
2017-12-18 09:12:17 +0200

    Add files via upload

---
+++ parser1.php
@@ -0,0 +1,69 @@
+<?php
+class Parser
+{
+	function parse(){
+ini_set('user_agent','Chrome');
+
+$url = 'http://www.investing.com/indices/uk-100/';
+$file = file_get_contents($url);
+
+$pattern = '#<span class="arial_26 inlineblock pid-27-last" id="last_last".+?</span>#';
+$pattern1 = '#<span class="lighterGrayFont noBold">Open:</span> <span dir="ltr".+?</span>#';
+
+
+preg_match($pattern, $file, $ftse);
+preg_match($pattern1, $file, $open);
+
+
+$fts=$ftse[0];
+$opn = $open[0];
+
+
+
+$fts = strip_tags($fts);
+$opn = strip_tags($opn);
+
+
+$fts = preg_replace('/[^.0-9]/', '', $fts);
+$opn= preg_replace('/[^.0-9]/', '', $opn);
+
+
+$float_open = floatval($opn);
+$float_fts = floatval($fts);
+
+$float_open1 = $float_open -1;
+
+$percent = ($float_fts / $float_open1) * 100;
+}
+
+class ParserResultQuery
+{
+	function insertinfo($fts,$open,$percent){
+		$serverName = "localhost";
+    $database = "shop";
+    $uid = 'root';
+    $pwd = '';
+
+$link="sqlsrv:server=$serverName;Database=$database";
+		$db = new PDO($link, 'root',''); 
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql="INSERT INTO tb_name ('fts','open','percent') VALUES ($float_fts,$float_open,$percent)";
+
+	$st=$db->prepare($sql);
+	if($st->execute()!=true){
+				return false;
+			} else {
+
+				$st=null;						
+				return true;
+			}	
+	
+
+insertinfo($float_fts,$float_open,$percent);
+	}
+}
+
+
+
+
+?>
